520b5e5afd010a034617dd25ce8d31559acdce80
Mohit Bhatt <mohitbhatt@hotmail.com>
2023-12-13 13:45:50 -0600

    Add files via upload

---
+++ MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿// MainViewModel.cs
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private int _rowCount;
+
+    public int RowCount
+    {
+        get => _rowCount;
+        set
+        {
+            _rowCount = value;
+            OnPropertyChanged();
+        }
+    }
+
+    // Implement INotifyPropertyChanged interface
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    // Add methods to load CSV, count rows, etc.
+}
